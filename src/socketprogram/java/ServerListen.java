b471896e1740984e63155879b0942717b90a75ab
abrahamgolla <abrahamgolla17@gmail.com>
2016-12-29 19:00:33 +0530

    Demo

---
+++ src/socketprogram/java/ServerListen.java
@@ -0,0 +1,29 @@
+package socketprogram.java;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListen {
+
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket serverSocket = new ServerSocket(9091);
+
+		System.out.println("waitig for a client on port "+serverSocket.getLocalPort());
+
+		Socket server = serverSocket.accept();
+
+		System.out.println("just connected to "+server.getRemoteSocketAddress());
+
+		DataInputStream input = new DataInputStream(server.getInputStream());
+
+		System.out.println(input.readLine());
+
+		server.close();
+
+		
+	}
+
+}
